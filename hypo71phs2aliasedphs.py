f37e2c2c5a5ed4becc498d5b85e1b6168d3ac3de
Raffaele Di Stefano <akt2971>
2023-08-30 18:18:24 +0200

    Utility to use the aliases file to also alias a hypo71 format h1 phase file

---
+++ hypo71phs2aliasedphs.py
@@ -0,0 +1,43 @@
+### IMPORTING LIBRARIES
+import os,argparse,subprocess,copy,pwd,socket,time
+import sys
+import pandas
+
+class MyParser(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write('error: %s\n' % message)
+        self.print_help()
+        sys.exit(2)
+
+def parseArguments():
+        parser=MyParser()
+        parser.add_argument('--aliases', help='aliases file output of fdsnws_station2hypoinverse')
+        parser.add_argument('--hypo71phs', help='hypo71 phase file of event(s)')
+        if len(sys.argv) <= 1:
+            parser.print_help()
+            sys.exit(1)
+        args=parser.parse_args()
+        return args
+
+################## MAIN ####################
+args=parseArguments()
+
+if os.path.isfile(args.aliases):
+   df_aliases = pandas.read_csv(args.aliases)
+
+if os.path.isfile(args.hypo71phs):
+   file_phs = open(args.hypo71phs,'r')
+
+hypo71phs = file_phs.readlines()
+
+for l in hypo71phs:
+    if len(l) > 1:
+       if l[77] == " ":
+          print(l[:76])
+       else:
+          print(l[:4]+l[77])
+    else:
+       print(l.rstrip())
+
+
+file_phs.close()
